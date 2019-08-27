4a9a4f9353552e80528e59fd46afc205b2e17e74
Xiaofei Zhang <xiaofeizhang@Xiaofeis-MacBook-Pro.local>
2019-08-27 14:58:21 +0100

    Add initialization with default balance

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,2 +1,10 @@
+require 'oystercard'
 describe Oystercard do
+
+  describe 'initialization' do
+    it 'has a default balance of 0' do
+      expect(subject.balance).to eq 0
+    end
+  end
+  
 end
