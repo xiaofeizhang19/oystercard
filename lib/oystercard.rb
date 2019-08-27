937d1efb1108f5b235475abfa5d721bfd24cc826
Sayem995 <sayem.bokhari.1@gmail.com>
2019-08-27 15:11:05 +0100

    added test and code for top_up and completed step 5

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,8 +1,13 @@
 class Oystercard
   attr_reader :balance
   DEFAULT_BALANCE = 0
-  
+
   def initialize(balance = DEFAULT_BALANCE)
     @balance = balance
   end
+
+  def top_up(amount)
+  end
+
+
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,10 +1,12 @@
 require 'oystercard'
 describe Oystercard do
-
-  describe 'initialization' do
-    it 'has a default balance of 0' do
-      expect(subject.balance).to eq 0
-    end
+  it 'has a default balance of 0' do
+    expect(subject.balance).to eq 0
   end
-  
+  describe '#top_up' do
+    it { is_expected.to respond_to(:top_up).with(1).argument }
+  end 
+
+
+
 end
